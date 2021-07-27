df0ef7aac71a0f7ad5502cb638b24fe50dd64da4
Andersson Paz <love.rem.saverem@gmail.com>
2021-07-27 14:57:54 -0500

    commit in remote

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'medicamecanica/git_tutorial',
+  ),
+  'versions' => 
+  array (
+    'erusev/parsedown' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '6598f3860c2698fe2f0f1bc98212fc01d0a1893c',
+    ),
+    'medicamecanica/git_tutorial' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
