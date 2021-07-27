dbc1c1575c70d41792ef4f24a82af2604c8d2a56
Ander <npander@hotmail.com>
2021-07-27 16:46:08 -0500

    se creo una tarjeta color primario dos filas run y test

--- index.php
+++ index.php
@@ -4,16 +4,38 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	</head>
 <body>
+	<div class="continer">
+		<div class="row">
+			<div class="offset-md-1 col col-md-8">
+				<?php
+				require_once 'vendor/autoload.php';
 
-<?php
-require_once 'vendor/autoload.php';
+				$file='README.md';
+				if(!is_file($file)) exit('README.md not found!'); 
+				$contents = file_get_contents($file);
+				$Parsedown = new Parsedown();
+				echo $Parsedown->text($contents);
+				?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="offset-md-1 col col-md-8">
+				<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+				 <div class="card-header">Card</div>
+				    <ul class=" list-group list-group-flush">
+							<li class="bg-primary list-group-item">run</li>
+							<li class="bg-primary list-group-item">test</li>
+							
+						  </ul>
+					<div class="card-body">
+						Version 2.1
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
 
-$file='README.md';
-if(!is_file($file)) exit('README.md not found!'); 
-$contents = file_get_contents($file);
-$Parsedown = new Parsedown();
-echo $Parsedown->text($contents);
-?>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
